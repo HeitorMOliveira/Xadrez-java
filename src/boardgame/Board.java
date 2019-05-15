9e86266d067df53591b3413f052170214dd767b1
Heitor Moreira <heitorm0rei1@gmail.com>
2019-05-15 17:27:16 -0300

    movimentação de peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -23,34 +23,52 @@ public class Board {
 	}
 
 	public Piece piece(int row, int column) {
-		if(!positionExists(row, column))throw new BoardException("Posição inválida");
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posicao invalida");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
-		if(!positionExists(position)) throw new BoardException("Posição inválida");
-		
-	return pieces[position.getRow()][position.getColumn()];
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao invalida");
+		}
+		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
-		if(thereIsAPiece(position)) throw new BoardException("Já tem uma peça na posição: "+ 	position);
+		if (thereIsAPiece(position)) {
+			throw new BoardException("ja ha uma peca na posicao " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
 	
 	public Piece removePiece(Position position) {
-		return null;
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao invalida");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
+	
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 	
 	public boolean thereIsAPiece(Position position) {
-		if(!positionExists(position)) throw new BoardException("Posição inválida");
+		if (!positionExists(position)) {
+			throw new BoardException("posicao invalida");
+		}
 		return piece(position) != null;
 	}
 }
